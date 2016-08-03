1c816e7093295a5789f5fea1422aaba3f3ed4515
Son Huy <sonhuy04@gmail.com>
2016-08-03 20:46:04 +0700

    move the code out of empty folder

--- main.js
+++ main.js
@@ -23,12 +23,30 @@ var mainState = {
 
 		// Add gravity to the bird to make it fall
 		this.bird.body.gravity.y = 1000;
+
+		// Call the 'jump' function when the spacekey is hit
+		var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		spaceKey.onDown.add(this.jump, this);
 	},
 
 	update: function() {
-		// This function is called 60 times per second
-		// It contains the game's logic
+		// If the bird is out of the screen (too high or too low)
+		// Call the 'restartGame' function
+		if (this.bird.y < 0 || this.bird.y > 490)
+			this.restartGame();
 	},
+
+	// Make the bird jump
+	jump: function() {
+		// Add a vertical velocity to the bird
+		this.bird.body.velocity.y = -350;
+	},
+
+	// Restart the game
+	restartGame: function() {
+		// Start the 'main' state, which restarts the game
+		game.state.start('main');
+	};
 };
 
 // Initialize Phaser, and create a 400px by 490px game
